c19e34add41b6a5e186732013689f1b7ff16750a
Lars Dansie <larswdansie@gmail.com>
2023-11-09 07:44:38 -0700

    final commit

---
+++ FavoriteBooks/BookTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  BookTableViewCell.swift
+//  FavoriteBooks
+//
+//  Created by Lars Dansie on 11/6/23.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var lengthLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+    
+    func update(with book: Book) {
+        titleLabel.text = book.title
+        authorLabel.text = book.author
+        genreLabel.text = book.genre
+        lengthLabel.text = book.length
+    }
+}
